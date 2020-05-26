4aadeeb339d4eac259293bbcad0ccc3918ea7d52
Ali Mohammadpour <aliemohammadpour@gmail.com>
2020-05-26 13:53:34 +0430

    refactor: Add Entity and Arch to Problem-52 Source

--- ProblemSets/PDS-ProblemSet-05/src/Problem52/resource_sharing.vhd
+++ ProblemSets/PDS-ProblemSet-05/src/Problem52/resource_sharing.vhd
@@ -15,30 +15,56 @@
 --  Additional Comments:
 --*/
 
-------------------------------------------------------------
--- type of condi_p and condi_q is boolean
--- type of other signals (ports) is std_logic_vector
--- Z1, Z2, Z3, and Z4 are output ports
-------------------------------------------------------------
-process(
-		A, B, C, D, E, F, G,
-		H, I, J, K, L, M, N,
-		condi_p, condi_q)
+
+entity resource_sharing is
+	port (
+		condi_p : in std_logic;
+		condi_q : in std_logic;
+
+		A  : in  std_logic_vector;
+		B  : in  std_logic_vector;
+		C  : in  std_logic_vector;
+		D  : in  std_logic_vector;
+		E  : in  std_logic_vector;
+		F  : in  std_logic_vector;
+		G  : in  std_logic_vector;
+		H  : in  std_logic_vector;
+		I  : in  std_logic_vector;
+		J  : in  std_logic_vector;
+		K  : in  std_logic_vector;
+		L  : in  std_logic_vector;
+		M  : in  std_logic_vector;
+		N  : in  std_logic_vector;
+		Z1 : out std_logic_vector;
+		Z2 : out std_logic_vector;
+		Z3 : out std_logic_vector;
+		Z4 : out std_logic_vector);
+	
+end resource_sharing;
+
+architecture resource_sharing_arc of resource_sharing is
 begin
-	Z1 <= A + B;
-	if(condi_p) then
-		Z2 <= C + D;
-	else
-		Z2 <= E + F;
-		if(condi_q) then
-			Z3 <= G + H;
+	process(
+			A, B, C, D, E, F, G,
+			H, I, J, K, L, M, N,
+			condi_p, condi_q)
+	begin
+		Z1 <= A + B;
+		if(condi_p) then
+			Z2 <= C + D;
 		else
-			Z3 <= I + J;
+			Z2 <= E + F;
+			if(condi_q) then
+				Z3 <= G + H;
+			else
+				Z3 <= I + J;
+			end if;
 		end if;
-	end if;
-	if(not condi_p) then
-		Z4 <= K + L;
-	else
-		Z4 <= M + N;
-	end if;
-end process;
+		if(not condi_p) then
+			Z4 <= K + L;
+		else
+			Z4 <= M + N;
+		end if;
+	end process;
+end resource_sharing_arc;
+
