1b3b34e3f5e7b6c95a99c4b74b30e46b876c2636
All Mohammadpour <aliemohammadpour@gmail.com>
2020-07-09 13:38:40 +0430

    feat: Project Template

---
+++ 2020-Spring/Project/Phase01/README.md
@@ -0,0 +1,12 @@
+# PDS-Project Phase 1
+
+
+## Testcase
+There is no VHDL format for each module to write the project, and the name and input of the modules can be customized. The input content of each module is shared. Use Module 3 output for Module 4 inputs.
+The inputs contained in this folder contain the contents of the various matrices we need.
+Consider value of R constant equal to 7 and value of L equal to 13.
+Size of Matrix A11 is 7X7.
+
+## Submission 
+For each module, one or more descriptive files, a test file, and the output content produced must be submitted. Put each module in its folder. If you need a description for each section, put a file with Radme in the module folder.
+
---
+++ 2020-Spring/Project/Phase01/pds_utils.vhd
@@ -0,0 +1,27 @@
+--/*
+--**********************************************************
+--  Design Automation Course Homework (Spring, 2020 Semester)
+--  Amirkabir University of Technology (Tehran Polytechnic)
+--  Department of Computer Engineering (CE-AUT)
+--  https://ce.aut.ac.ir
+--  Designed TA (ali[dot]mohammadpour[at]ac[dot]ir)
+--  *******************************************************
+--  Student ID  : XXXXXXX
+--  Student ID  : XXXXXXX
+--  Student Name: -----------------
+--  Student Name: -----------------
+--  Student Mail: -----------------
+--  Student Mail: -----------------
+--  *******************************************************
+--  Module: PDS Project : PDS Utilities Package
+--  *******************************************************
+--  Additional Comments:
+--*/
+
+package pds_utils is
+
+end package pds_utils;
+
+package body pds_utils is
+
+end package body pds_utils;
