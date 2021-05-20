9f5bfb0c486cb5f54b59ebddf91b7cd283b8b0c9
Mahan Zendedel DH <Mahanmmi@gmail.com>
2021-05-20 18:39:15 +0430

    Fixed syntax error in tb_tanh and tb_xdsp

--- 2021-Spring/ProblemSets/ProblemSet04/src/tb_tanh.vhd
+++ 2021-Spring/ProblemSets/ProblemSet04/src/tb_tanh.vhd
@@ -1,32 +1,28 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-library xil_defaullib;
-use xil_defaullib.xpoint_pkg.all;
+library xil_defaultlib;
+use xil_defaultlib.xpoint_pkg.all;
 
 entity tb_tanh is
 end tb_tanh;
 
 architecture testbench of tb_tanh is
-
-	component tanh is
-		port (
-			x : in  xfixed( 2 downto -3);
-			outp : out std_logic_vector(15 downto 0);
-	end component xdsp;
-
-	signal x : xfixed( 2 downto -3);
-	signal out_lutx : std_logic_vector(15 downto 0);
-	signal out_mult : std_logic_vector(15 downto 0);
+component tanh is
+	port (
+		x : in  xfixed( 2 downto -3);
+		outp : out std_logic_vector(15 downto 0));
+end component tanh;
+signal x : xfixed( 2 downto -3);
+signal out_lutx : std_logic_vector(15 downto 0);
+signal out_mult : std_logic_vector(15 downto 0);
 begin
-		tanh_lutx : port map (
-			x => x,
-			outp => out_lutx
-		);
-		tanh_mult : port map (
-			x => x,
-			outp => out_mult
-		);
-
-
+	tanh_lutx : tanh port map (
+		x => x,
+		outp => out_lutx
+	);
+	tanh_mult : tanh port map (
+		x => x,
+		outp => out_mult
+	);
 end testbench;
--- 2021-Spring/ProblemSets/ProblemSet04/src/tb_xdsp.vhd
+++ 2021-Spring/ProblemSets/ProblemSet04/src/tb_xdsp.vhd
@@ -1,30 +1,26 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-library xil_defaullib;
-use xil_defaullib.xpoint_pkg.all;
+library xil_defaultlib;
+use xil_defaultlib.xpoint_pkg.all;
 
 entity tb_xdsp is
 end tb_xdsp;
 
 architecture testbench of tb_xdsp is
-
-	component xdsp is
-		port (
-			inpX : in  xfixed( 7 downto -8);
-			inpY : in  xfixed( 7 downto -8);
-			outp : out xfixed(15 downto -8);
-	end component xdsp;
-
-	signal x : xfixed( 7 downto -8);
-	signal y : xfixed( 7 downto -8);
-	signal o : xfixed(15 downto -8);
+component xdsp is
+	port (
+		inpX : in  xfixed( 7 downto -8);
+		inpY : in  xfixed( 7 downto -8);
+		outp : out xfixed(15 downto -8));
+end component xdsp;
+signal x : xfixed( 7 downto -8);
+signal y : xfixed( 7 downto -8);
+signal o : xfixed(15 downto -8);
 begin
-		xdsp_unit : port map (
-			inpX => x,
-			inpY => y,
-			outp => o
-		);
-
+	xdsp_unit : xdsp port map (
+		inpX => x,
+		inpY => y,
+		outp => o
+	);
 end testbench;
-
